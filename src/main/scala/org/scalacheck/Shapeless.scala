8f6530505e580cb40c823dc7707565a8c656ca4d
Alexandre Archambault <alexandre.archambault@gmail.com>
2015-04-11 15:22:02 +0200

    Taking generation size into account
    - Propagating it to product elements
    - Limiting coproduct generation size (prevents arbitrary stack overflows)

--- src/main/scala/org/scalacheck/Shapeless.scala
+++ src/main/scala/org/scalacheck/Shapeless.scala
@@ -1,7 +1,8 @@
 package org.scalacheck
 
 import shapeless._
-import shapeless.ops.coproduct.Length
+import shapeless.ops.hlist.{ Length => HListLength }
+import shapeless.ops.coproduct.{ Length => CoproductLength }
 import shapeless.ops.nat.ToInt
 
 object Shapeless {
@@ -9,15 +10,29 @@ object Shapeless {
   implicit val hnilArbitrary: Arbitrary[HNil] =
     Arbitrary(Gen.const(HNil))
   
-  implicit def hconsArbitrary[H, T <: HList](implicit
+  implicit def hconsArbitrary[H, T <: HList, N <: Nat](implicit
     headArbitrary: Lazy[Arbitrary[H]],
-    tailArbitrary: Lazy[Arbitrary[T]]      
+    tailArbitrary: Lazy[Arbitrary[T]],
+    length: HListLength.Aux[T, N],
+    n: ToInt[N]
   ): Arbitrary[H :: T] =
     Arbitrary {
-      for {
-        h <- Gen.lzy(headArbitrary.value.arbitrary)
-        t <- Gen.lzy(tailArbitrary.value.arbitrary)
-      } yield h :: t
+      Gen.sized { size =>
+        val sig = math.signum(size)
+        val remainder = sig * size % (n() + 1)
+        val fromRemainderGen =
+          if (remainder > 0)
+            Gen.choose(1, n()).map(r => if (r <= remainder) sig else 0)
+          else
+            Gen.const(0)
+
+        for {
+          fromRemainder <- fromRemainderGen
+          headSize = size / (n() + 1) + fromRemainder
+          head <- Gen.resize(headSize, Gen.lzy(headArbitrary.value.arbitrary))
+          tail <- Gen.resize(size - headSize, Gen.lzy(tailArbitrary.value.arbitrary))
+        } yield head :: tail
+      }
     }
   
   implicit val cnilArbitrary: Arbitrary[CNil] = Arbitrary(Gen.fail)
@@ -25,14 +40,20 @@ object Shapeless {
   implicit def cconsEquallyWeightedArbitrary[H, T <: Coproduct, N <: Nat](implicit
     headArbitrary: Lazy[Arbitrary[H]],
     tailArbitrary: Lazy[Arbitrary[T]],
-    length: Length.Aux[T, N],
+    length: CoproductLength.Aux[T, N],
     n: ToInt[N]
   ): Arbitrary[H :+: T] =
     Arbitrary {
-      Gen.frequency(
-        1 -> Gen.lzy(headArbitrary.value.arbitrary).map(Inl(_)),
-        n() -> Gen.lzy(tailArbitrary.value.arbitrary).map(Inr(_))
-      )
+      Gen.sized {
+        case 0 => Gen.fail
+        case size =>
+          val sig = math.signum(size)
+
+          Gen.frequency(
+            1   -> Gen.resize(size - sig, Gen.lzy(headArbitrary.value.arbitrary)).map(Inl(_)),
+            n() -> Gen.resize(size - sig, Gen.lzy(tailArbitrary.value.arbitrary)).map(Inr(_))
+          )
+      }
     }
 
   implicit def instanceArbitrary[F, G](implicit

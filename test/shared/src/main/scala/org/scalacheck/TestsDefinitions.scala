d98a013201793492e20f892a427adb7dd9d841c6
Alexandre Archambault <alexandre.archambault@gmail.com>
2016-05-14 16:22:25 +0200

    Move things around a bit in test project

---
+++ test/shared/src/main/scala/org/scalacheck/TestsDefinitions.scala
@@ -0,0 +1,73 @@
+package org.scalacheck
+
+import shapeless.{ Lazy => _, _ }
+import shapeless.record._
+import shapeless.union._
+
+object TestsDefinitions {
+
+  case class Simple(i: Int, s: String, blah: Boolean)
+
+  case object Empty
+  case class EmptyCC()
+  case class Composed(foo: Simple, other: String)
+  case class TwiceComposed(foo: Simple, bar: Composed, v: Int)
+  case class ComposedOptList(fooOpt: Option[Simple], other: String, l: List[TwiceComposed])
+
+  sealed trait Base
+  case class BaseIS(i: Int, s: String) extends Base
+  case class BaseDB(d: Double, b: Boolean) extends Base
+  case class BaseLast(c: Simple) extends Base
+
+  case class CCWithSingleton(i: Int, s: Witness.`"aa"`.T)
+
+  sealed trait BaseWithSingleton
+  object BaseWithSingleton {
+    case class Main(s: Witness.`"aa"`.T) extends BaseWithSingleton
+    case class Dummy(i: Int) extends BaseWithSingleton
+  }
+
+  object T1 {
+    sealed abstract class Tree
+    final case class Node(left: Tree, right: Tree, v: Int) extends Tree
+    case object Leaf extends Tree
+  }
+
+  object T2 {
+    sealed abstract class Tree
+    case class Node(left: Tree, right: Tree, v: Int) extends Tree
+    case object Leaf extends Tree
+  }
+
+  // cvogt's hierarchy
+  sealed trait A
+  sealed case class B(i: Int, s: String) extends A
+  case object C extends A
+  sealed trait D extends A
+  final case class E(a: Double, b: Option[Float]) extends D
+  case object F extends D
+  sealed abstract class Foo extends D
+  case object Baz extends Foo
+  // Not supporting this one
+  // final class Bar extends Foo
+  // final class Baz(i1: Int)(s1: String) extends Foo
+
+  type L = Int :: String :: HNil
+  type Rec = Record.`'i -> Int, 's -> String`.T
+
+  type C0 = Int :+: String :+: CNil
+  type Un = Union.`'i -> Int, 's -> String`.T
+
+
+  object NoTCDefinitions {
+    trait NoArbitraryType
+    case class ShouldHaveNoArb(n: NoArbitraryType, i: Int)
+    case class ShouldHaveNoArbEither(s: String, i: Int, n: NoArbitraryType)
+
+    sealed trait BaseNoArb
+    case class BaseNoArbIS(i: Int, s: String) extends BaseNoArb
+    case class BaseNoArbDB(d: Double, b: Boolean) extends BaseNoArb
+    case class BaseNoArbN(n: NoArbitraryType) extends BaseNoArb
+  }
+
+}
--- test/shared/src/test/scala/org/scalacheck/SingletonsTests.scala
+++ test/shared/src/test/scala/org/scalacheck/SingletonsTests.scala
@@ -7,26 +7,6 @@ import utest._
 
 import Util.validateSingletons
 
-object SingletonsTestsDefinitions {
-
-  // Running into SI-7046 for Base if these are put directly into SingletonsTests
-
-  case object CaseObj
-  case class Empty()
-
-  sealed trait Base
-  case object BaseObj extends Base
-  case class BaseEmpty() extends Base
-
-  sealed trait BaseMore
-  case object BaseMoreObj extends BaseMore
-  case class BaseMoreEmpty() extends BaseMore
-  case class BaseMoreNonSingleton(i: Int) extends BaseMore
-
-  case class NonSingleton(s: String)
-
-}
-
 object SingletonsTests extends TestSuite {
   import SingletonsTestsDefinitions._
 

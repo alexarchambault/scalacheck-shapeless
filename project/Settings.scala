dd02af0efd5a05f71eb501c89167ef156ccb6da9
Alexandre Archambault <alexarchambault@users.noreply.github.com>
2018-08-11 18:26:53 +0200

    Set up sbt-ci-release (#87)

--- project/Settings.scala
+++ project/Settings.scala
@@ -20,39 +20,6 @@ object Settings {
         case _ =>
           Nil
       }
-    },
-    organization := "com.github.alexarchambault",
-    homepage := Some(url("https://github.com/alexarchambault/scalacheck-shapeless")),
-    licenses := Seq(
-      "Apache 2.0" -> url("http://opensource.org/licenses/Apache-2.0")
-    ),
-    scmInfo := Some(ScmInfo(
-      url("https://github.com/alexarchambault/scalacheck-shapeless.git"),
-      "scm:git:github.com/alexarchambault/scalacheck-shapeless.git",
-      Some("scm:git:git@github.com:alexarchambault/scalacheck-shapeless.git")
-    )),
-    developers := List(Developer(
-      "alexarchambault",
-      "Alexandre Archambault",
-      "",
-      url("https://github.com/alexarchambault")
-    )),
-    publishMavenStyle := true,
-    pomIncludeRepository := { _ => false },
-    publishTo := Some {
-      val nexus = "https://oss.sonatype.org/"
-      if (isSnapshot.value)
-        "snapshots" at nexus + "content/repositories/snapshots"
-      else
-        "releases" at nexus + "service/local/staging/deploy/maven2"
-    },
-    credentials ++= {
-      Seq("SONATYPE_USER", "SONATYPE_PASS").map(sys.env.get) match {
-        case Seq(Some(user), Some(pass)) =>
-          Seq(Credentials("Sonatype Nexus Repository Manager", "oss.sonatype.org", user, pass))
-        case _ =>
-          Seq()
-      }
     }
   )
 

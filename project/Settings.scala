b2b1b4ae741983db80994658b6d83a49a5a56da6
Alexandre Archambault <alexarchambault@users.noreply.github.com>
2018-08-19 22:35:09 +0200

    Set crossScalaVersions (#89)

--- project/Settings.scala
+++ project/Settings.scala
@@ -5,7 +5,13 @@ import Aliases._
 
 object Settings {
 
+  private def scala210 = "2.10.6"
+  private def scala211 = "2.11.12"
+  private def scala212 = "2.12.4"
+
   lazy val shared = Seq(
+    scalaVersion := scala212,
+    crossScalaVersions := Seq(scala212, scala211, scala210),
     resolvers += Resolver.sonatypeRepo("releases"),
     libs ++= {
       if (scalaBinaryVersion.value == "2.10")

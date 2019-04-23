5d4909d6667174be366235cd55c3e7ce1a7173a0
Alexandre Archambault <alexandre.archambault@gmail.com>
2019-04-23 09:27:58 +0200

    Switch to scala 2.13.0-RC1

--- project/Deps.scala
+++ project/Deps.scala
@@ -8,5 +8,5 @@ object Deps {
 
   def scalacheck = setting("org.scalacheck" %%% "scalacheck" % "1.14.0")
   def shapeless = setting("com.chuusai" %%% "shapeless" % "2.3.3")
-  def utest = setting("com.lihaoyi" %%% "utest" % "0.6.6")
+  def utest = setting("com.lihaoyi" %%% "utest" % "0.6.7")
 }

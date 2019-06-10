294ff5ce33dddc820de071849e851ea6ee3b02c3
Alexandre Archambault <alexarchambault@users.noreply.github.com>
2019-06-10 18:38:38 +0200

    Switch to scala 2.13.0 (#106)

--- project/Deps.scala
+++ project/Deps.scala
@@ -1,4 +1,5 @@
 import sbt._
+import sbt.Keys.scalaVersion
 
 import org.portablescala.sbtplatformdeps.PlatformDepsPlugin.autoImport._
 
@@ -8,5 +9,11 @@ object Deps {
 
   def scalacheck = setting("org.scalacheck" %%% "scalacheck" % "1.14.0")
   def shapeless = setting("com.chuusai" %%% "shapeless" % "2.3.3")
-  def utest = setting("com.lihaoyi" %%% "utest" % "0.6.7")
+  def utest = setting {
+    val sv = scalaVersion.value
+    val ver =
+      if (sv.startsWith("2.10.") || sv.startsWith("2.11.")) "0.6.7"
+      else "0.6.9"
+    "com.lihaoyi" %%% "utest" % ver
+  }
 }

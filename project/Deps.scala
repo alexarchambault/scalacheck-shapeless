3fbf8598437b3b906d35e1d3736657104fa561ef
Scala Steward <me@scala-steward.org>
2022-04-08 10:06:20 +0200

    Update scalacheck to 1.16.0

--- project/Deps.scala
+++ project/Deps.scala
@@ -7,7 +7,7 @@ object Deps {
 
   import Def.setting
 
-  def scalacheck = setting("org.scalacheck" %%% "scalacheck" % "1.15.4")
+  def scalacheck = setting("org.scalacheck" %%% "scalacheck" % "1.16.0")
   def shapeless = setting("com.chuusai" %%% "shapeless" % "2.3.7")
   def utest = setting("com.lihaoyi" %%% "utest" % "0.7.10")
 }

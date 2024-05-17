113fc80a0c01942b2830d91362f734ea82d43cc1
Scala Steward <scala_steward@virtuslab.com>
2024-05-17 13:24:05 +0000

    Update shapeless to 2.3.11

--- src/config.scala
+++ src/config.scala
@@ -1,7 +1,7 @@
 //> using scala "2.13.10", "2.13.10"
 //> using platform "jvm", "scala-js", "native"
 //> using lib "org.scalacheck::scalacheck::1.17.0"
-//> using lib "com.chuusai::shapeless::2.3.10"
+//> using lib "com.chuusai::shapeless::2.3.11"
 //> using jvm "8"
 
 //> using publish.organization "com.github.alexarchambault"

3db07c837a82ad344fb7322ec166e66ecdc8a82b
Scala Steward <scala_steward@virtuslab.com>
2025-11-26 14:53:53 +0000

    Update scala-library to 2.13.18

--- src/config.scala
+++ src/config.scala
@@ -1,4 +1,4 @@
-//> using scala "2.13.14", "2.12.19"
+//> using scala "2.13.18", "2.12.19"
 //> using platform "jvm", "scala-js", "native"
 //> using lib "org.scalacheck::scalacheck::1.18.0"
 //> using lib "com.chuusai::shapeless::2.3.12"
